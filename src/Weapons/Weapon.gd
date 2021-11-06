56c45bf707cdc599b26dedf848b0d57f9d9f838a
Chris Racioppo <Gravedigger7789@gmail.com>
2021-11-05 23:03:46 -0400

    Simplified weapon

--- src/Weapons/Weapon.gd
+++ src/Weapons/Weapon.gd
@@ -5,16 +5,10 @@ const Projectile := preload("res://src/Weapons/Projectile.tscn")
 
 onready var cooldown_timer := $Cooldown
 
-var can_attack := true
-
 func attack() -> void:
-	if can_attack:
-		can_attack = false
+	if cooldown_timer.is_stopped():
 		cooldown_timer.start()
 		var projectile = Projectile.instance()
 		projectile.global_position = global_position
 		projectile.set_as_toplevel(true)
 		add_child(projectile)
-
-func _on_Cooldown_timeout() -> void:
-	can_attack = true
