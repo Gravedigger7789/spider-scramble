97f603f875bac0be7cb1b9672d6eb229c8a564ac
Chris Racioppo <Gravedigger7789@gmail.com>
2021-11-05 22:17:36 -0400

    Adding types to parameters

--- src/Weapons/Projectile.gd
+++ src/Weapons/Projectile.gd
@@ -4,10 +4,10 @@ extends Area2D
 const SPEED := 500.0
 const ENEMY_LAYER := 2
 
-func _physics_process(delta) -> void:
+func _physics_process(delta: float) -> void:
 	position.x += SPEED * delta
 
-func _on_Projectile_body_entered(body) -> void:
+func _on_Projectile_body_entered(body: Node) -> void:
 	if body.collision_layer == ENEMY_LAYER:
 		body.queue_free()
 	queue_free()
