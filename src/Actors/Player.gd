592cea0ceee36b448ca1b2927a8c10ffcb1b7ac3
Chris Racioppo <Gravedigger7789@gmail.com>
2021-11-09 22:48:29 -0500

    Adjusted jump speed and added jump interrupt

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 class_name Player
 
-const JUMP_SPEED := 400.0
+const JUMP_SPEED := 450.0
 const TERMINAL_VELOCITY := 500.0
 
 var velocity := Vector2()
@@ -17,6 +17,8 @@ func _physics_process(delta: float) -> void:
 	
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y = -JUMP_SPEED
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y *= 0.6;
 	
 	if Input.is_action_just_pressed("attack"):
 		weapon.attack()
