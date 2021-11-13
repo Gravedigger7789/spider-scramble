dcff49df963289fbedb342592120aecb7ba44e68
Chris Racioppo <Gravedigger7789@gmail.com>
2021-11-13 18:51:49 -0500

    Renamed Level folder

--- src/Level/ScrollingParallaxBackground.gd
+++ src/Level/ScrollingParallaxBackground.gd
@@ -4,6 +4,6 @@ extends ParallaxBackground
 export var velocity: Vector2 = Vector2( -400, 0 );
  
  
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	var offset: Vector2 = get_scroll_offset() + velocity * delta
 	set_scroll_offset(offset)
