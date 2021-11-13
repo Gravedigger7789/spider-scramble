ba79aac5b35cc1876222a22ac2c77ea79ce4e007
Chris Racioppo <Gravedigger7789@gmail.com>
2021-11-13 15:52:37 -0500

    Added typing

--- src/Objects/Obstacle.gd
+++ src/Objects/Obstacle.gd
@@ -1,9 +1,10 @@
 extends Area2D
 class_name Obstacle
 
-const SPEED = 400.0
+const SPEED := 400.0
+
 onready var sprite: Sprite = $Sprite
-onready var size = Vector2(sprite.texture.get_width(), sprite.texture.get_height())
+onready var size := Vector2(sprite.texture.get_width(), sprite.texture.get_height())
 
 
 func _physics_process(delta: float) -> void:
@@ -14,5 +15,5 @@ func _on_Obstacle_body_entered(body: Node) -> void:
 		queue_free()
 		body.take_damage()
 
-func _on_VisibilityNotifier2D_screen_exited():
+func _on_VisibilityNotifier2D_screen_exited() -> void:
 	queue_free()
