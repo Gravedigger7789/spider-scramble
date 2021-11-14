f8bdbf94c93f22a8948da70f9cf6fa359dc0d7e2
Chris Racioppo <Gravedigger7789@gmail.com>
2021-11-14 00:12:49 -0500

    Parallax Forest Background (#10)
    * Working on parallax background
    
    * Renamed Level folder
    
    * Fixed oversampling warning
    
    * Added sprites to tree background
    
    * Working on sprite spawners
    
    * Moved obstacle spawn off screen
    
    * Formatting
    
    * Changed background name

---
+++ src/Level/TreeBackground.gd
@@ -0,0 +1,18 @@
+extends ParallaxBackground
+
+export var velocity: Vector2 = Vector2(-400, 0)
+
+onready var tree_spawner: SpriteSpawner = $TreeSpawner
+onready var shrub_spawner: SpriteSpawner = $ShrubSpawner
+onready var foliage_spawner: SpriteSpawner = $FoliageSpawner
+
+
+func _ready():
+	tree_spawner.sprite_velocity = velocity
+	shrub_spawner.sprite_velocity = velocity
+	foliage_spawner.sprite_velocity = velocity
+
+
+func _physics_process(delta: float) -> void:
+	var offset: Vector2 = get_scroll_offset() + velocity * delta
+	set_scroll_offset(offset)
