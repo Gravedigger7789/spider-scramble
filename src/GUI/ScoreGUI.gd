dcdf70352bb723be1c493bb753297d2a6760b904
Chris Racioppo <Gravedigger7789@gmail.com>
2021-11-21 16:20:59 -0500

    Fixed coin count

--- src/GUI/ScoreGUI.gd
+++ src/GUI/ScoreGUI.gd
@@ -2,16 +2,17 @@ extends HBoxContainer
 
 onready var label: Label = $CenterContainer/Label
 var score := 0.0 setget set_score
+var coins := 0
 
 
 func set_score(value: float) -> void:
 	if score != value:
 		score = value
-		label.text = "%.0f" % score
+		label.text = "%.0f" % (score + coins)
 
 
-func _on_Player_coins_changed(coins):
-	self.score += coins
+func _on_Player_coins_changed(value):
+	coins = value
 
 
 func _process(delta):
