88a3898ccf2b2fd152be06e2769e47ce3111ce72
Chris Racioppo <Gravedigger7789@gmail.com>
2021-11-21 19:30:27 -0500

    Adding game over menu

--- src/GUI/GameGUI.gd
+++ src/GUI/GameGUI.gd
@@ -6,9 +6,10 @@ onready var health_gui = $HBoxContainer/PanelContainer/HealthGUI
 onready var ammo_gui = $HBoxContainer/PanelContainer2/AmmoGUI
 onready var score_gui = $HBoxContainer/PanelContainer3/ScoreGUI
 onready var pause_menu = $CanvasLayer/PauseMenu
+onready var game_over_menu = $CanvasLayer/GameOverMenu
 
 
-func _ready():
+func _ready() -> void:
 	var connected = player.connect(
 		"max_health_changed", health_gui, "_on_Player_max_health_changed"
 	)
@@ -17,6 +18,9 @@ func _ready():
 	connected = player.connect("health_changed", health_gui, "_on_Player_health_changed")
 	if connected != OK:
 		push_warning("Could not connect player health changed")
+	connected = player.connect("health_depleted", self, "_on_Player_health_depleted")
+	if connected != OK:
+		push_warning("Could not connect player health depleted")
 	connected = player.connect("max_ammo_changed", ammo_gui, "_on_Player_max_ammo_changed")
 	if connected != OK:
 		push_warning("Could not connect player max ammo changed")
@@ -28,7 +32,11 @@ func _ready():
 		push_warning("Could not connect player coins changed")
 
 
-func _unhandled_input(event):
+func _unhandled_input(event) -> void:
 	if event.is_action_pressed("pause"):
 		pause_menu.show()
 		get_tree().set_input_as_handled()
+
+
+func _on_Player_health_depleted() -> void:
+	game_over_menu.show()
