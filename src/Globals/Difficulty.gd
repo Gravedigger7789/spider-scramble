c8b2c5f1d5ec93f2a1543ecc3edb1bf8278066af
Chris Racioppo <Gravedigger7789@gmail.com>
2021-11-21 19:48:41 -0500

    Static typing

--- src/Globals/Difficulty.gd
+++ src/Globals/Difficulty.gd
@@ -6,7 +6,7 @@ var difficulty_increase_seconds := 5.0
 var difficulty_increase = 0.1
 var elapsed_time := 0.0
 
-func _process(delta):
+func _process(delta: float) -> void:
 	elapsed_time += delta
 	if elapsed_time >= difficulty_increase_seconds:
 		speed_modifier += difficulty_increase
