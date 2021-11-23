f307f24377f03465247c05ef3f20de3401d428d8
Chris Racioppo <Gravedigger7789@gmail.com>
2021-11-22 23:32:24 -0500

    Added splash screen

---
+++ src/Level/SplashScreen.gd
@@ -0,0 +1,21 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+#	get_tree().paused = true
+#	$Player.set_physics_process(false)
+#	$Player.set_process(false)
+#	$Player.set_process_input(false)
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
