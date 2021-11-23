835b93bce29ae376fef653ac458ac39b3ebb864c
Chris Racioppo <Gravedigger7789@gmail.com>
2021-11-23 01:17:14 -0500

    Syntax

--- src/Level/SplashScreen.gd
+++ src/Level/SplashScreen.gd
@@ -1,15 +1,13 @@
 extends Node
 
-
-export (PackedScene) var first_scene := preload("res://src/Level/Sandbox1.tscn")
+export(PackedScene) var first_scene := preload("res://src/Level/Sandbox1.tscn")
 
 
 func _input(event: InputEvent) -> void:
 	if (
 		event.is_action_pressed("attack")
-		or event.is_action_pressed("jump")
-		or event.is_action_pressed("pause")
-		or event is InputEventMouseButton
+		|| event.is_action_pressed("jump")
+		|| event.is_action_pressed("pause")
 	):
 		get_tree().set_input_as_handled()
 		get_tree().change_scene_to(first_scene)
