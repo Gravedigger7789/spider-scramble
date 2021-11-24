399910ac2baef9e3667aad076ceba96c3dca9518
Chris Racioppo <Gravedigger7789@gmail.com>
2021-11-23 23:31:09 -0500

    Changing to single theme

--- src/Level/Game.gd
+++ src/Level/Game.gd
@@ -1,15 +1,6 @@
 extends Node
 
-export(Array, AudioStreamSample) var loop_sounds: Array
-
-onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
-
 
 func _ready() -> void:
 	Difficulty.reset()
 	randomize()
-
-
-func _on_AudioStreamPlayer_finished() -> void:
-	audio_stream_player.stream = loop_sounds[randi() % loop_sounds.size()]
-	audio_stream_player.play()
