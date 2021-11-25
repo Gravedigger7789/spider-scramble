0b19692354559b77b19413e2d873045e2f7437c3
Chris Racioppo <Gravedigger7789@gmail.com>
2021-11-25 01:40:24 -0500

    Adding elapsed time debug

--- src/GUI/DebugDifficultyGUI.gd
+++ src/GUI/DebugDifficultyGUI.gd
@@ -4,13 +4,16 @@ extends VBoxContainer
 const LEVEL_TEXT = "Level: %-5.0f"
 const SPEED_TEXT = "Speed Multiplier: %-5.2f"
 const SPAWN_TEXT = "Spawn Multiplier: %-5.2f"
+const ELAPSED_TEXT = "Time Since Last Level: %-5.2f"
 
 onready var difficulty_level = $DifficultyLevel
 onready var speed_multiplier = $SpeedMultiplier
 onready var spawn_multiplier = $SpawnMultiplier
+onready var elapsed_time = $ElapsedTime
 
 
 func _process(_delta):
 	difficulty_level.text = LEVEL_TEXT % Difficulty.difficulty_level
 	speed_multiplier.text = SPEED_TEXT % Difficulty.speed_modifier
 	spawn_multiplier.text = SPAWN_TEXT % Difficulty.spawn_modifier
+	elapsed_time.text = ELAPSED_TEXT % Difficulty.elapsed_time
